92a12fa335b2ea40f4918c3fe1dce8f19ad5307c
nemoola <144327005+nemoola@users.noreply.github.com>
2024-07-27 18:17:24 +0900

    WIP

--- commands/weather.go
+++ commands/weather.go
@@ -23,6 +23,28 @@ func (Weather) Execute(s *discordgo.Session, m *discordgo.MessageCreate, args []
 			_, _ = s.ChannelMessageSend(m.ChannelID, err.Error())
 			return
 		}
-		_, _ = s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("%.1f℃", amedas.Temp[0]))
+		embed := discordgo.MessageEmbed{}
+		embed.Title = fmt.Sprintf("%sの天気情報", args[0])
+		embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+			Name:  "天気",
+			Value: fmt.Sprintf("WIP"),
+		})
+		embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+			Name:  "気温",
+			Value: fmt.Sprintf("%.1f℃", amedas.Temp[0]),
+		})
+		embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+			Name:  "降水量(10分)",
+			Value: fmt.Sprintf("%.1fmm", amedas.Precipitation10m[0]),
+		})
+		embed.Fields = append(embed.Fields, &discordgo.MessageEmbedField{
+			Name:  "風速",
+			Value: fmt.Sprintf("%.1fm/s", amedas.WindDirection[0]),
+		})
+		embed.Color = 0x00ff00
+		_, err = s.ChannelMessageSendEmbed(m.ChannelID, &embed)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
